595e13eaad637a6746e065957a94a259f2a4a623
selman <gunduzgece546@gmail.com>
2022-06-12 00:45:34 +0300

    theme input

--- app.js
+++ app.js
@@ -1,31 +1,89 @@
 const theme= document.getElementById('theme-img');
-let sun= true;
+let sun;
 window.addEventListener('load',()=>{
-  theme.setAttribute('data-theme','true')
+  let theme = localStorage.getItem('theme');
+  if(theme==null) {
+    localStorage.setItem('theme',true);
+    sun = true;
+  }
+  sun = Boolean(theme) ;
+  
+  
+  
 })
+function changeTheme() {
+  if(sun==true){
+    //karanlık modu
+    theme.src= './images/icon-sun.svg';
+    document.querySelector('body').style.backgroundImage= 'url(./images/bg-desktop-dark.jpg)';
+    document.querySelector('body').style.backgroundColor ='var(--dt-very-dark-blue)';
+    document.querySelector('#add-div').style.backgroundColor = 'var(--dt-very-dark-desaturated-blue)';
+    
+    
+
+
+
+    
+   
+  }else {
+    // aydınlık modu
+    theme.src= './images/icon-moon.svg';
+    document.querySelector('body').style.backgroundImage= 'url(./images/bg-desktop-light.jpg)';
+    document.querySelector('body').style.backgroundColor ='var(--lt-very-light-gray)';
+    document.querySelector('#add-div').style.backgroundColor = 'var(--lt-very-light-gray)';
+    
+    
+    
+    sun = true;
+    localStorage.setItem('theme',sun)
+  }
+  console.log(sun);
+}
 
 theme.addEventListener('click',()=>{
   if(sun==true){
     //karanlık modu
     theme.src= './images/icon-sun.svg';
     document.querySelector('body').style.backgroundImage= 'url(./images/bg-desktop-dark.jpg)';
-    document.querySelector('body').style.backgroundColor ='var(--dt-very-dark-blue)'
+    document.querySelector('body').style.backgroundColor ='var(--dt-very-dark-blue)';
+    document.querySelector('#add-div').style.backgroundColor = 'var(--dt-very-dark-desaturated-blue)';
+    document.querySelectorAll('.check').forEach((e)=>{
+      e.style.border='1px solid var(--dt-very-dark-grayish-blue)'
+    });
+    document.querySelector('.todo-container').style.backgroundColor ='var(--dt-very-dark-desaturated-blue)';
+    document.querySelectorAll('.todo-item').forEach((e)=>{
+      e.style.borderBottom ='1px solid var(--dt-very-dark-grayish-blue)'
+    });
+    document.querySelector('.todo-container').style.boxShadow = '0px 2px 100px -40px var(--dt-very-dark-desaturated-blue)';
     
     
-
-
-
+    
+    
+    
+    
     sun = false;
+    localStorage.setItem('theme',sun);
   }else {
     // aydınlık modu
     theme.src= './images/icon-moon.svg';
     document.querySelector('body').style.backgroundImage= 'url(./images/bg-desktop-light.jpg)';
-    document.querySelector('body').style.backgroundColor ='var(--lt-very-light-gray)'
-
-
+    document.querySelector('body').style.backgroundColor ='var(--lt-very-light-gray)';
+    document.querySelector('#add-div').style.backgroundColor = 'var(--lt-very-light-gray)';
+    document.querySelector('.check').style.border='1px solid var(--lt-very-light-grayish-blue)';
+    document.querySelector('.todo-container').style.backgroundColor ='white';
+    document.querySelectorAll('.check').forEach((e)=>{
+      e.style.border='1px solid var(--lt-very-light-grayish-blue)'
+    });
+    document.querySelectorAll('.todo-item').forEach((e)=>{
+      e.style.borderBottom ='1px solid var(--lt-very-light-grayish-blue)'
+    });
+    document.querySelector('.todo-container').style.boxShadow = '0px 2px 100px -40px var(--lt-dark-grayish-blue)';
+    
+    
     sun = true;
+    localStorage.setItem('theme',sun)
   }
   console.log(sun);
   
-  
+
 })
